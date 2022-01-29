3873485006cb254e8c2ad325ea2c84f411b70602
vasilii yatsyuk <72764693+mad-zephyr@users.noreply.github.com>
2022-01-30 00:33:14 +0200

    developed user page and feature to add/delete comments

--- src/app/components/pages/userPage/userCard.jsx
+++ src/app/components/pages/userPage/userCard.jsx
@@ -10,17 +10,24 @@ const UserCard = () => {
   const users = JSON.parse(window.localStorage.getItem('users'))
   const user = users.find(user => user._id === userId)
 
+  const style = {
+    position: 'sticky',
+    top: '20px'
+  }
+
   return (
     <div className="container">
       <div className="row g-3">
-      <div className="col-md-4 mb-2">
-        <Card user={user}/>
-        <QualityCard qulities={user.qualities} />
-        <MeetingsCard meetings={user.completedMeetings}/>
-      </div>
-      <div className="col-md-8">
-        <ComponentComment />
-      </div>
+        <div className="col-md-4 mb-2">
+          <div style={style}>
+            <Card user={user}/>
+            <QualityCard qulities={user.qualities} />
+            <MeetingsCard meetings={user.completedMeetings}/>
+          </div>
+        </div>
+        <div className="col-md-8">
+          <ComponentComment />
+        </div>
       </div>
     </div>
   )
